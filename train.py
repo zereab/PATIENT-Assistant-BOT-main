afd13873f8e1ad0a7c3ff3577f415c31c509ebff
zereab <86871337+zereab@users.noreply.github.com>
2021-08-18 02:41:56 -0700

    Add files via upload

---
+++ train.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pyrebase
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+import joblib
+
+df_train = pd.read_csv('dataset/Training.csv', delimiter=',')
+df_test = pd.read_csv('dataset/Testing.csv', delimiter=',')
+nRow, nCol = df_train.shape
+
+label = preprocessing.LabelEncoder()
+label.fit(pd.concat([df_train['prognosis'], df_test['prognosis']]))
+labels = label.fit_transform(df_train['prognosis'])
+symptoms = df_train[df_train.columns.difference(['prognosis'])]
+
+#model = RandomForestClassifier()
+model = GaussianNB()
+model.fit(symptoms, labels)
+joblib.dump(model, 'model.pkl')
+
